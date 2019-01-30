8335cbbdaffee1d1b505f81967a2dbbb0d83ba03
rawanah995 <rawanah995@gmail.com>
2019-01-30 16:10:56 +0300

    last version

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-
-// function play(){
-    //     $("td").text("X");
-    
-    // }
- var winningIndex = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [3, 4, 5], [6, 7, 8], [2, 4, 6]];
-
-var table=$("td");
-//var qs = new Querystring();
+var winningIndex = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [3, 4, 5], [6, 7, 8], [2, 4, 6]];
 var gameType;
 gameType ="two-players";
-console.log(gameType);
-var player="X";
 var play=true;
-var score ={
-    "X":0,"O":0,"Tie":0};
-    $("#x-score").text(score["X"]);
+var player="X";
+var score ={"X":0, "O":0, "Tie":0};
+$("#x-score").text(score["X"]);
 $("#o-score").text(score["O"]);
 $("#tie").text(score["Tie"]);
-console.log(score["X"]);
-
 $("#two-players").css("background-color","gray");
 $("#easy").on("click",easyclicked);
 $("#hard").on("click",hardclicked);
+$("#intermediate").on("click",intermediateclicked);
 $("#two-players").on("click",twoplayersclicked);
 
 function easyclicked(){
+    if (gameType=="two-players"){
+        reset_score();
+    }
     gameType="easy";
     play=true;
     $("td").text("");
@@ -32,49 +24,82 @@ function easyclicked(){
     $("#easy").css("background-color","gray"); 
 }
 function hardclicked(){
+    if (gameType=="two-players"){
+        reset_score();
+    }
     gameType="hard";
     play=true;
     $("td").text("");
     $("button").css("background-color","white"); 
     $("#hard").css("background-color","gray"); 
 }
+function intermediateclicked(){
+    if (gameType=="two-players"){
+        reset_score();
+    }
+    gameType="intermediate";
+    play=true;
+    $("td").text("");
+    $("button").css("background-color","white"); 
+    $("#intermediate").css("background-color","gray"); 
+}
 function twoplayersclicked(){
+    if (!gameType=="two-players"){
+      reset_score();
+    }
     gameType="two-players";
     play=true;
     $("td").text("");
     $("button").css("background-color","white"); 
     $("#two-players").css("background-color","gray"); 
 }
+function reset_score(){
+    score["X"]=0;
+    score["O"]=0;
+    score["Tie"]=0;
+$("#x-score").text(score["X"]);
+$("#o-score").text(score["O"]);
+$("#tie").text(score["Tie"]);
+
+}
 
 
 $("td").click(function () {
+    //winner();
+    if(play===true){
     if($(event.target).text()==""){
     if(gameType=="two-players"){
     var tdId=jQuery(this).attr("id");
-    if($("#"+tdId).text()=="" && play==true){
+    if($(event.target).text()=="" ){
     $("#"+tdId).text(player);}
     if(player=="X"){
     player="O";}
 else{
     player="X"
 }
-winner();
-
 }
-
+winner();
 if(gameType=="easy"){
     $(event.target).text("X");
-   winner();
+    winner();  
     random();
-    winner();}
+    winner();
+   }
 
-if(gameType=="hard"){
-  
+
+if(gameType=="hard" || gameType=="intermediate"){
     $(event.target).text("X");
     winner();
     computerwins();
     winner();
-}}});
+   }
+   
+}}
+//winner();
+}
+);
+
+
 
 
 
@@ -91,23 +116,24 @@ index[6]=$("#seven");
 index[7]=$("#eight");
 index[8]=$("#nine");
 var randomArray=[];
-for (var i=0; i<table.length;i++){
-    if( index[i].text()==""){
+for (var i=0; i<index.length;i++){
+    if( index[i].text()=="")
+{
        randomArray.push(index[i]);
-}
-}
+}}
 
 randomArray[Math.floor(Math.random()*randomArray.length)].text("O");
+winner();
 }
+
+
 
 
 $(".playAgain").click(function () {
     play=true;
 $("td").text("");
-
-
 });
-//$("td").on("click",play);
+
 function winner(){
     var index=[];
 index[0]=$("#one").text();
@@ -120,72 +146,109 @@ index[6]=$("#seven").text();
 index[7]=$("#eight").text();
 index[8]=$("#nine").text();
 
-for (var i=0; i<index.length;i++){
-    console.log(index[i]);
+for (var i=0;i<winningIndex.length;i++){
+    var index1=index[winningIndex[i][0]];
+    var index2=index[winningIndex[i][1]];
+    var index3=index[winningIndex[i][2]];
+if(index1==index2 && index2==index3){
+    if(index1 !="" && play==true){
+            score[index1]++;
+            play=false; 
+            alert("The winner is: "+index1);
+}}
 }
 
-
-if(index[0]==index[1] && index[1]==index[2]){
-    if(index[0]!="" && play==true){
-        score[index[0]]++;
-        play=false; 
-    alert("The winner is: "+index[0]);
-}
-}
- else
-if(index[0]==index[4] && index[4]==index[8]){
-    if(index[0]!="" && play==true){
-        score[index[0]]++;
-        play=false; 
-        alert("The winner is: "+index[0]);
-    }
-}
-else
-if(index[2]==index[4] && index[4]==index[6]){
-    if(index[2]!="" && play==true ){
-        score[index[2]]++;
-        play=false; 
-        alert("The winner is: "+index[2]);}
-} else
-if(index[2]==index[5] && index[5]==index[8]){
-    if(index[2]!="" && play==true){
-        score[index[2]]++;
-        play=false; 
-        alert("The winner is: "+index[2]);}  
-}else
-if(index[1]==index[4] && index[4]==index[7]){
-    if(index[1]!="" && play==true){
-        score[index[1]]++;
-        play=false; 
-        alert("The winner is: "+index[1]);}
-}else
-if(index[0]==index[3] && index[3]==index[6]){
-    if(index[0]!="" && play==true){
-        score[index[0]]++;
-        play=false; 
-        alert("The winner is: "+index[0]);}
-}else
-if(index[3]==index[4] && index[4]==index[5]){
-    if(index[3]!="" && play==true){
-        score[index[3]]++;
-        play=false; 
-        alert("The winner is: "+index[3]);}
-}else
-if(index[6]==index[7] && index[7]==index[8]){
-    if(index[6]!="" && play==true){
-        score[index[6]]++;
-        play=false; 
-        alert("The winner is: "+index[6]);}}
-        else
-     if (!index.includes("")){
+if (!index.includes("") && play==true ){
     score["Tie"]++;
-    alert("Tie");
+            play=false; 
+            alert("Tie");
 }
 $("#x-score").text(score["X"]);
 $("#o-score").text(score["O"]);
 $("#tie").text(score["Tie"]);
-
 }
+
+
+
+// function winner2(){
+//     var index=[];
+// index[0]=$("#one").text();
+// index[1]=$("#two").text();
+// index[2]=$("#three").text();
+// index[3]=$("#four").text();
+// index[4]=$("#five").text();
+// index[5]=$("#six").text();
+// index[6]=$("#seven").text();
+// index[7]=$("#eight").text();
+// index[8]=$("#nine").text();
+
+// if(index[0]==index[1] && index[1]==index[2]){
+//     if(index[0]!="" && play==true){
+//         score[index[0]]++;
+//         play=false; 
+//         alert("The winner is: "+index[0]);
+     
+// }
+// }
+//  else
+// if(index[0]==index[4] && index[4]==index[8]){
+//     if(index[0]!="" && play==true){
+//         score[index[0]]++;
+//         play=false; 
+//        alert("The winner is: "+index[0]);
+        
+//     }
+// }
+// else
+// if(index[2]==index[4] && index[4]==index[6]){
+//     if(index[2]!="" && play==true ){
+//         score[index[2]]++;
+//         play=false; 
+//         alert("The winner is: "+index[2]);}
+// }
+//  else
+// if(index[2]==index[5] && index[5]==index[8]){
+//     if(index[2]!="" && play==true){
+//         score[index[2]]++;
+//         play=false; 
+//         alert("The winner is: "+index[2]);}  
+// }
+// else
+// if(index[1]==index[4] && index[4]==index[7]){
+//     if(index[1]!="" && play==true){
+//         score[index[1]]++;
+//         play=false; 
+//         alert("The winner is: "+index[1]);}
+// }
+// else
+// if(index[0]==index[3] && index[3]==index[6]){
+//     if(index[0]!="" && play==true){
+//         score[index[0]]++;
+//         play=false; 
+//        alert("The winner is: "+index[0]);}
+// }
+// else
+// if(index[3]==index[4] && index[4]==index[5]){
+//     if(index[3]!="" && play==true){
+//         score[index[3]]++;
+//         play=false; 
+//         alert("The winner is: "+index[3]);}
+// }
+// else if(index[6]==index[7] && index[7]==index[8]){
+//     if(index[6]!="" && play==true){
+//         score[index[6]]++;
+//         play=false; 
+//         alert("The winner is: "+index[6]);}}
+//         else
+//      if (!index.includes("")){
+//     score["Tie"]++;
+//     alert("Tie");
+// }
+// $("#x-score").text(score["X"]);
+// $("#o-score").text(score["O"]);
+// $("#tie").text(score["Tie"]);
+
+// }
 
 
 
@@ -207,13 +270,27 @@ function computerwins() {
 
     if (index[4].text() === "") {
         index[4].text("O");
-    }else if(index[5].text()=="X" && index[7].text() && index[8].text()==""){
+        // I think this position is the best
+    }else
+    if(index[5].text()=="X" && index[7].text()=="X" && index[8].text()=="" && gameType=="hard"){
 
         index[8].text("O");
-    }
+        return;
+/*
+__|__|__
+__|__|X
+__|X |X
+
+this condition will check if the x has two possibilities
+
+    */  
+     }
+
+
      else {
         var tempArr = [];
         var tempArr2 = [];
+
         for (var i = 0; i < winningIndex.length; i++) {
             tempArr[0] = index[winningIndex[i][0]];
             tempArr[1] = index[winningIndex[i][1]];
@@ -221,16 +298,14 @@ function computerwins() {
             tempArr2[0] = index[winningIndex[i][0]].text();
             tempArr2[1] = index[winningIndex[i][1]].text();
             tempArr2[2] = index[winningIndex[i][2]].text();
-            //console.log(winning[i][0]);
-            //console.log(winning[i][1]);
-            //console.log(winning[i][2]);
-
             
-if (!tempArr2.includes("X") && tempArr2.includes("")){
-    {var countO=0;
+if (!tempArr2.includes("X") && tempArr2.includes(""))
+  
+    {
+var countO=0;
 for(var j=0;j<tempArr2.length;j++)
     if(tempArr2[j]=="O"){
-        
+         //this condition will check if there is two O  and will win if there is  
         countO++;
 if(countO==2){
     console.log("I am winning");
@@ -251,6 +326,7 @@ for (var i = 0; i < winningIndex.length; i++) {
     tempArr2[1] = index[winningIndex[i][1]].text();
     tempArr2[2] = index[winningIndex[i][2]].text();
 if (tempArr2.includes("X") && tempArr2.includes("") && !tempArr2.includes("O")){
+        //this condition will check if there is two X and doesn't let the X win 
     {var countX=0;
     for(var j=0;j<tempArr2.length;j++)
 if(tempArr2[j]=="X"){
@@ -273,7 +349,7 @@ return;
     tempArr2[2] = index[winningIndex[i][2]].text();
 
             if (!tempArr2.includes("X") && tempArr2.includes("")) {
-               // console.log(tempArr);
+                 // if there is two O and there is no X try to win 
                console.log("try to win");
                 var ind = tempArr2.indexOf("");
                 tempArr[ind].text("O");
@@ -281,4 +357,4 @@ return;
             }
         }
         random();
-    }}
+    }
